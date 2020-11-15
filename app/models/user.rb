5ae8cbbeeb54d64fa24855f404f2c07c3f1b8b29
Nimu-263 <junnishimura26@gmail.com>
2020-11-16 08:06:05 +0900

    バリデーションとエラーメッセージを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  letters = /\A[ぁ-んァ-ン一-龥]+\z/
+  kana = /\A[ァ-ン]+\z/
+  letters_numbers = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :nickname,          presence: true
+  validates :password,          presence: true, format: { with: letters_numbers, message: "Include both letters and numbers"}
+  validates :first_name,        presence: true, format: { with: letters, message: "Full-width characters"}
+  validates :family_name,       presence: true, format: { with: letters, message: "Full-width characters"}
+  validates :first_name_kana,   presence: true, format: { with: kana, message: "Full-width katakana characters"}
+  validates :family_name_kana,  presence: true, format: { with: kana, message: "Full-width katakana characters"}
+  validates :birth_day,         presence: true
 end
