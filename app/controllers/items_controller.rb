6d24c06e2f9ebe9a729d27e0655b8fdea086ca47
Nimu-263 <junnishimura26@gmail.com>
2020-11-21 17:42:14 +0900

    コントローラを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    binding.pry
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:explanation,:category_id,:condition_id,:delivery_fee_burden_id,:delivery_source_id,:delivery_days_id,:price)
+  end
 end
