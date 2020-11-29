28fc40389107f832194e11dd9aba04e5b6896314
Nimu-263 <junnishimura26@gmail.com>
2020-11-29 21:27:24 +0900

    ログインページに遷移するbefore_actionをitem_controllerに記述
    上記に加えご指摘いただいた部分を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :new, :create, :show]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -45,8 +46,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == item.user.id
+    unless current_user.id == @item.user.id
       redirect_to action: :index
     end
   end
