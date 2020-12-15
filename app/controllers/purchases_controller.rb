aa1cfe6c0521f34a7b1d7d2b878f10df8826ccce
Nimu-263 <junnishimura26@gmail.com>
2020-12-15 17:03:46 +0900

    クレジットカード決済機能を追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,23 +1,36 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
     @user_purchase = UserPurchase.new
-    @item = Item.find(params[:item_id])
   end
  
   def create
     @user_purchase = UserPurchase.new(purchase_params)
       if @user_purchase.valid?
+        pay_item
         @user_purchase.save
         redirect_to root_path
       else
-        @item = Item.find(params[:item_id])
         render action: :index
       end
   end
  
   private
   def purchase_params
-   params.require(:user_purchase).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+   params.require(:user_purchase).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,10 +1,11 @@
 class UserPurchase
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
+    validates :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
     validates :city
     validates :address
     validates :phone_number, numericality: { only_integer: true, message: "Input only number" }
